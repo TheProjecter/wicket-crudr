37b194becd2d35742b6ca5fcfc28aa6869df8356
nicktarix@gmail.com <nicktarix@gmail.com@5ac8de6a-6e9f-d5df-8d9c-067c972b89df>
2012-04-16 14:47:53 +0000

    Adding new test test currently fails

--- trunk/src/test/java/net/unbewaff/wicketcrudr/columns/TestFlexibleEditableColumn.java
+++ trunk/src/test/java/net/unbewaff/wicketcrudr/columns/TestFlexibleEditableColumn.java
@@ -17,6 +17,7 @@ import net.unbewaff.wicketcrudr.providers.TextFieldProvider;
 
 import org.apache.wicket.extensions.markup.html.repeater.data.table.DataTable;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
+import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.repeater.data.ListDataProvider;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.util.tester.WicketTester;
@@ -83,6 +84,35 @@ public class TestFlexibleEditableColumn {
         panel.add(table);
         tester.startComponentInPage(panel);
         tester.executeAjaxEvent("test:table:body:rows:1:cells:1:cell:label", "onclick");
+        tester.assertInvisible("test:table:body:rows:1:cells:1:cell:label");
+        tester.assertVisible("test:table:body:rows:2:cells:1:cell:label");
+        tester.assertVisible("test:table:body:rows:3:cells:1:cell:label");
+        tester.assertVisible("test:table:body:rows:1:cells:1:cell:editor");
+        tester.assertInvisible("test:table:body:rows:2:cells:1:cell:editor");
+        tester.assertInvisible("test:table:body:rows:3:cells:1:cell:editor");
+        tester.assertLabel("test:table:body:rows:2:cells:1:cell:label", "Banana");
+        tester.assertLabel("test:table:body:rows:3:cells:1:cell:label", "Cherry");
+    }
+
+    @Test
+    public void testDisplayEditing() {
+        List<IColumn<StringHolder>> cols = new ArrayList<IColumn<StringHolder>>();
+        ILabelProvider<StringHolder> labelProvider = new SimpleLabelProvider<StringHolder>();
+        IEditorProvider<StringHolder> editorProvider = new TextFieldProvider<StringHolder>();
+        ISurroundingContainerProvider containerProvider = new TextFieldPanelProvider();
+        ContainerConfiguration<StringHolder> conf = new ContainerConfiguration<StringHolder>(labelProvider, editorProvider, containerProvider, "data");
+        cols.add(new FlexibleEditableColumn<StringHolder>(Model.of("data"), conf));
+
+        DataTable<StringHolder> table = new DataTable<StringHolder>("table", cols, new ListDataProvider<StringHolder>(data), 5);
+        TempPanel panel = new TempPanel("test");
+        panel.add(table);
+        tester.startComponentInPage(panel);
+        tester.executeAjaxEvent("test:table:body:rows:1:cells:1:cell:label", "onclick");
+        tester.assertComponent("test:table:body:rows:1:cells:1:cell:editor:editor", TextField.class);
+        TextField tf = (TextField) tester.getComponentFromLastRenderedPage("test:table:body:rows:1:cells:1:cell:editor:editor");
+        String[] value = {"Apricot"};
+        tf.setModelValue(value);
+        tester.executeAjaxEvent("test:table:body:rows:1:cells:1:cell:editor:editor", "onblur");
         tester.assertInvisible("test:table:body:rows:1:cells:1:cell:label");
         tester.assertVisible("test:table:body:rows:2:cells:1:cell:label");
         tester.assertVisible("test:table:body:rows:3:cells:1:cell:label");
