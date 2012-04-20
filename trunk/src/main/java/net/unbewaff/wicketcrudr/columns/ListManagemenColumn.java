5e872ac473515f521cb8f9fe1043aa4d99c46d06
nicktarix@gmail.com <nicktarix@gmail.com@5ac8de6a-6e9f-d5df-8d9c-067c972b89df>
2012-04-20 14:51:01 +0000

    Extracted LinkPanel from ListManagementColumn still needs markup

--- trunk/src/main/java/net/unbewaff/wicketcrudr/columns/ListManagemenColumn.java
+++ trunk/src/main/java/net/unbewaff/wicketcrudr/columns/ListManagemenColumn.java
@@ -5,14 +5,9 @@ package net.unbewaff.wicketcrudr.columns;
 
 import net.unbewaff.wicketcrudr.dataproviders.IManageableDataProvider;
 import net.unbewaff.wicketcrudr.events.AjaxEventPayload;
-import net.unbewaff.wicketcrudr.events.ListChangedEventPayLoad;
 
-import org.apache.wicket.ajax.AjaxRequestTarget;
-import org.apache.wicket.ajax.markup.html.AjaxFallbackLink;
-import org.apache.wicket.event.Broadcast;
 import org.apache.wicket.extensions.markup.html.repeater.data.grid.ICellPopulator;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.AbstractColumn;
-import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.markup.repeater.Item;
 import org.apache.wicket.markup.repeater.data.IDataProvider;
 import org.apache.wicket.model.IModel;
@@ -52,42 +47,7 @@ public class ListManagemenColumn<T> extends AbstractColumn<T> {
     @Override
     public void populateItem(Item<ICellPopulator<T>> cellItem, String componentId, final IModel<T> rowModel) {
 
-        cellItem.add(new Panel(componentId, rowModel) {
-
-            @Override
-            protected void onInitialize() {
-                add(new AjaxFallbackLink<T>("delete", rowModel) {
-                    @Override
-                    public void onClick(AjaxRequestTarget target) {
-                        provider.delete(getModelObject());
-                        send(getPage(), Broadcast.DEPTH, new ListChangedEventPayLoad<T>(target, provider));
-                    }
-                });
-                add(new AjaxFallbackLink<T>("first", rowModel) {
-                    @Override
-                    public void onClick(AjaxRequestTarget target) {
-                        provider.first(getModelObject());
-                        send(getPage(), Broadcast.DEPTH, new ListChangedEventPayLoad<T>(target, provider));
-                    }
-                });
-                add(new AjaxFallbackLink<T>("up", rowModel) {
-                    @Override
-                    public void onClick(AjaxRequestTarget target) {
-                        provider.up(getModelObject());
-                        send(getPage(), Broadcast.DEPTH, new ListChangedEventPayLoad<T>(target, provider));
-                    }
-                });
-                add(new AjaxFallbackLink<T>("down", rowModel) {
-                    @Override
-                    public void onClick(AjaxRequestTarget target) {
-                        provider.down(getModelObject());
-                        send(getPage(), Broadcast.DEPTH, new ListChangedEventPayLoad<T>(target, provider));
-                    }
-                });
-
-                super.onInitialize();
-            }
-        });
+        cellItem.add(new LinkPanel<T>(componentId, rowModel, provider));
     }
 
 }
