9034c76279da3e74d74d57467d2d8e7141cfadef
nicktarix@gmail.com <nicktarix@gmail.com@5ac8de6a-6e9f-d5df-8d9c-067c972b89df>
2012-04-25 08:01:11 +0000

    A little refactoring

--- trunk/src/main/java/net/unbewaff/wicketcrudr/columns/ColumnFactory.java
+++ trunk/src/main/java/net/unbewaff/wicketcrudr/columns/ColumnFactory.java
@@ -10,6 +10,7 @@ import net.unbewaff.wicketcrudr.annotations.Lister;
 
 import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.PropertyColumn;
+import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.model.StringResourceModel;
 
@@ -26,19 +27,36 @@ public class ColumnFactory implements Serializable {
     public static <T> IColumn<T> getColumn(Lister l, Editor e, String property, Class<T> clazz) {
         String cleanProperty = getCleanPropertyName(property);
         IColumn<T> col = null;
-        String display = clazz.getSimpleName() + "." + cleanProperty;
-        if (!"".equals(l.headerKey())) {
-        	display = l.headerKey();
-        }
+        IModel<String> displayModel = getHeaderModel(l.headerKey(), clazz.getSimpleName(), cleanProperty);
         if (!l.editInPlace()) {
-			col = new PropertyColumn<T>(new StringResourceModel(display, Model.of(""), cleanProperty), cleanProperty);
+            col = new PropertyColumn<T>(displayModel, cleanProperty);
         }
         return col;
     }
 
+    /**
+     * @param headerKey
+     * @param clazzName
+     * @param cleanProperty
+     * @return
+     */
+    private static IModel<String> getHeaderModel(String headerKey, String clazzName, String cleanProperty) {
+        String display;
+        if (!"".equals(headerKey)) {
+        	display = headerKey;
+        } else {
+            display = clazzName + "." + cleanProperty;
+        }
+        return new StringResourceModel(display, Model.of(""), cleanProperty);
+    }
+
+
+
     private static String getCleanPropertyName(String property) {
         String clean = property;
         if (clean.startsWith("get")) {
+            clean = clean.substring(3);
+        } else if (clean.startsWith("has")) {
             clean = clean.substring(3);
         } else if (clean.startsWith("is")) {
             clean = clean.substring(2);
