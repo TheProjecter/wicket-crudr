f7097f598228a0eac4220a5bfd6a0045fe735231
nicktarix@gmail.com <nicktarix@gmail.com@5ac8de6a-6e9f-d5df-8d9c-067c972b89df>
2012-04-25 09:49:24 +0000

    Rebased StringResourceColumn to PropertyColumn Switched Ancestry from AbstractColumn to PropertyColumn and reimplemented functionality

--- trunk/src/main/java/net/unbewaff/wicketcrudr/columns/StringResourceColumn.java
+++ trunk/src/main/java/net/unbewaff/wicketcrudr/columns/StringResourceColumn.java
@@ -3,42 +3,34 @@
  */
 package net.unbewaff.wicketcrudr.columns;
 
-import org.apache.wicket.Component;
-import org.apache.wicket.extensions.markup.html.repeater.data.grid.ICellPopulator;
-import org.apache.wicket.extensions.markup.html.repeater.data.table.AbstractColumn;
-import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.repeater.Item;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.PropertyColumn;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.model.StringResourceModel;
 
 /**
- * @author DavidH
+ * Extension to {@link PropertyColumn<T>} to provide a StringResourceModel to the Label
+ *
+ * @author David Hendrix (Nicktarix)
  *
  */
-public class StringResourceColumn<T> extends AbstractColumn<T> {
+public class StringResourceColumn<T> extends PropertyColumn<T> {
 
-	private static final long serialVersionUID = 358092732279936235L;
-	private String prefix;
-	private String propertyExpression;
-	private Component parent;
+    private static final long serialVersionUID = 740826471748724758L;
+    private  String resourceKey;
 
-	public StringResourceColumn(IModel<String> displayModel, String sortProperty, String prefix,
-			String propertyExpression, Component parent) {
-		super(displayModel, sortProperty);
-		this.prefix = prefix;
-		this.propertyExpression = propertyExpression;
-		this.parent = parent;
-	}
+    public StringResourceColumn(IModel<String> displayModel, String sortProperty, String propertyExpression, String resourceKey) {
+        super(displayModel, sortProperty, propertyExpression);
+    }
 
-	public void populateItem(Item<ICellPopulator<T>> cellItem, String componentId, IModel<T> rowModel) {
-		PropertyModel<?> model = new PropertyModel<Object>(rowModel, propertyExpression);
-		String defaultValue = (String) model.getObject();
-		if (defaultValue == null) {
-			defaultValue = "";
-		}
-		StringResourceModel stringresource = new StringResourceModel(prefix, parent, model, defaultValue);
-		cellItem.add(new Label(componentId, stringresource.getString()));
-	}
+    public StringResourceColumn(IModel<String> displayModel, String propertyExpression, String resourceKey) {
+        this(displayModel, null, propertyExpression, resourceKey);
+    }
+
+    @Override
+    protected IModel<?> createLabelModel(IModel<T> rowModel) {
+        IModel<String> model = new StringResourceModel(resourceKey, new PropertyModel<Object>(rowModel, getPropertyExpression()));
+        return model;
+    }
 
 }
