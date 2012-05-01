4c5c089ab87edf96699514d4cd8a7a41cb1e62be
nicktarix@gmail.com <nicktarix@gmail.com@5ac8de6a-6e9f-d5df-8d9c-067c972b89df>
2012-05-01 20:43:45 +0000

    Added convenience method

--- trunk/src/main/java/net/unbewaff/wicketcrudr/providers/label/LabelProviderFactory.java
+++ trunk/src/main/java/net/unbewaff/wicketcrudr/providers/label/LabelProviderFactory.java
@@ -9,7 +9,7 @@ import net.unbewaff.wicketcrudr.annotations.Lister;
 import net.unbewaff.wicketcrudr.providers.labelmodel.ILabelModelProvider;
 
 /**
- * Factory to create ILabelProviderObjects based on Metadata
+ * Factory to create {@link net.unbewaff.wicketcrudr.providers.label.ILabelProvider<T>}Objects based on Metadata
  *
  * @author David Hendrix (Nicktarix)
  *
@@ -20,10 +20,26 @@ public class LabelProviderFactory {
         // static use only
     }
     
+    /**
+     * Wrapper to {@link net.unbewaff.wicketcrudr.providers.label.LabelProviderFactory.getLabelProvider(Lister, ILabelModelProvider<T>)} 
+     * to simplify API usage by providing a Method instead of multiple Annotations
+     * 
+     * @param <T>
+     * @param m The annotated Method
+     * @param labelModelProvider The {@link net.unbewaff.wicketcrudr.providers.labelmodel.ILabelModelProvider<T>}
+     * @return The matching {@link net.unbewaff.wicketcrudr.providers.label.ILabelProvider<T>}
+     */
     public static <T> ILabelProvider<T> getLabelProvider(Method m, ILabelModelProvider<T> labelModelProvider) {
     	return getLabelProvider(m.getAnnotation(Lister.class), labelModelProvider);
     }
 
+    /**
+     * The real factory that provides matching {@link net.unbewaff.wicketcrudr.providers.label.ILabelProvider<T>} instances based on Metadata
+     * @param <T>
+     * @param l the {@link net.unbewaff.wicketcrudr.annotations.Lister} from the current method
+     * @param labelModelProvider The {@link net.unbewaff.wicketcrudr.providers.labelmodel.ILabelModelProvider<T>}
+     * @return The matching {@link net.unbewaff.wicketcrudr.providers.label.ILabelProvider<T>}
+     */
     @SuppressWarnings("unchecked")
     public static <T> ILabelProvider<T> getLabelProvider(Lister l, ILabelModelProvider<T> labelModelProvider) {
         ILabelProvider<T> provider = null;
