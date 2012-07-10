68ec7ef786472043d687be59ab266730eb999510
nicktarix@gmail.com <nicktarix@gmail.com@5ac8de6a-6e9f-d5df-8d9c-067c972b89df>
2012-07-10 14:17:12 +0000

    Petclinic demo dataclasses

---
+++ trunk/src/main/java/net/unbewaff/wicketcrudr/demo/petclinic/dataclasses/Specialities.java
@@ -0,0 +1,11 @@
+package net.unbewaff.wicketcrudr.demo.petclinic.dataclasses;
+
+/**
+ * @author David Hendrix (Nicktarix)
+ *
+ */
+public enum Specialities {
+    radiology,
+    surgery,
+    dentistry;
+}
