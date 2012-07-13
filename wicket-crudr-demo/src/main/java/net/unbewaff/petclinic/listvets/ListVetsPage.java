5117dda6440a9ddaa5228f1bb11914729413238e
nicktarix <nicktarix@gmail.com>
2012-07-13 13:42:43 +0000

    more work on demo app

--- wicket-crudr-demo/src/main/java/net/unbewaff/petclinic/listvets/ListVetsPage.java
+++ wicket-crudr-demo/src/main/java/net/unbewaff/petclinic/listvets/ListVetsPage.java
@@ -3,8 +3,16 @@
  */
 package net.unbewaff.petclinic.listvets;
 
+import java.util.List;
+
+import net.unbewaff.petclinic.WebSession;
+import net.unbewaff.petclinic.entities.Veterinarian;
+import net.unbewaff.wicketcrudr.components.AutoLister;
+import net.unbewaff.wicketcrudr.components.ICrudrListProvider;
+
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 /**
@@ -35,6 +43,8 @@ public class ListVetsPage extends WebPage {
 
 	@Override
 	protected void onInitialize() {
+		VetListProvider listProvider = new VetListProvider(((WebSession)getSession()).getVeterinarians());
+		add(new AutoLister<VetWrapper>("vetsList", Model.of(listProvider), 5, VetWrapper.class));
 		super.onInitialize();
 	}
 
