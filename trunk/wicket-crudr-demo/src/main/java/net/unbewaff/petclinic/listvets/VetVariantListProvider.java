0282af1fe503b1aa114dbc20e826ab34958ee35b
nicktarix@gmail.com <nicktarix@gmail.com@5ac8de6a-6e9f-d5df-8d9c-067c972b89df>
2012-07-19 12:09:53 +0000

    Used extended display capabilities

---
+++ trunk/wicket-crudr-demo/src/main/java/net/unbewaff/petclinic/listvets/VetVariantListProvider.java
@@ -0,0 +1,34 @@
+/**
+ *
+ */
+package net.unbewaff.petclinic.listvets;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import net.unbewaff.petclinic.entities.Veterinarian;
+import net.unbewaff.wicketcrudr.components.ICrudrListProvider;
+
+/**
+ * @author DavidH
+ *
+ */
+public class VetVariantListProvider implements ICrudrListProvider<VetWrapperVariant>, Serializable {
+
+	private final List<VetWrapperVariant> list = new ArrayList<VetWrapperVariant>();
+
+	public VetVariantListProvider(List<Veterinarian> list) {
+		for (Veterinarian vet : list) {
+			this.list.add(new VetWrapperVariant(vet));
+		}
+	}
+	/* (non-Javadoc)
+	 * @see net.unbewaff.wicketcrudr.components.ICrudrListProvider#getList()
+	 */
+	@Override
+	public List<VetWrapperVariant> getList() {
+		return list;
+	}
+
+}
---
+++ trunk/wicket-crudr-demo/src/main/java/net/unbewaff/petclinic/listvets/VetWrapperVariant.java
@@ -0,0 +1,35 @@
+/**
+ *
+ */
+package net.unbewaff.petclinic.listvets;
+
+import java.io.Serializable;
+import java.util.Set;
+
+import net.unbewaff.petclinic.entities.Veterinarian;
+import net.unbewaff.petclinic.listvets.VetWrapper.SpecialitiesWrapper;
+import net.unbewaff.wicketcrudr.annotations.InnerType;
+import net.unbewaff.wicketcrudr.annotations.InnerType.DisplayType;
+import net.unbewaff.wicketcrudr.annotations.Lister;
+
+/**
+ * @author davidh
+ *
+ */
+public class VetWrapperVariant extends VetWrapper implements Serializable {
+
+	/**
+	 * @param vet
+	 */
+	public VetWrapperVariant(Veterinarian vet) {
+		super(vet);
+	}
+
+	@Override
+	@Lister
+	@InnerType(resourcePrefix="Speciality", type=SpecialitiesWrapper.class, displayAs=DisplayType.CONCATENATED)
+	public Set<SpecialitiesWrapper> getSpecialities() {
+		return super.getSpecialities();
+	}
+
+}
