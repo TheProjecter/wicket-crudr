0705946f54a8c6b607510c16d4b0d73f3f33c6e5
nicktarix <nicktarix@gmail.com>
2012-07-25 20:43:11 +0000

    Made StringResourceModelProvider Serializable again, added a Test

--- wicket-crudr-core/src/main/java/net/unbewaff/wicketcrudr/providers/labelmodel/LabelModelProviderFactory.java
+++ wicket-crudr-core/src/main/java/net/unbewaff/wicketcrudr/providers/labelmodel/LabelModelProviderFactory.java
@@ -5,12 +5,12 @@ package net.unbewaff.wicketcrudr.providers.labelmodel;
 
 import java.lang.reflect.Method;
 
-import org.apache.log4j.Logger;
-
 import net.unbewaff.wicketcrudr.annotations.InnerType;
 import net.unbewaff.wicketcrudr.annotations.Lister;
 import net.unbewaff.wicketcrudr.annotations.Lister.Display;
 import net.unbewaff.wicketcrudr.annotations.ResourceKey;
+
+import org.apache.log4j.Logger;
 
 
 /**
@@ -42,27 +42,8 @@ public class LabelModelProviderFactory {
     }
 
     public static <T> ILabelModelProvider<T> getLabelModelProvider(String resourcePrefix, InnerType innerType) {
-    	Method resourceProvider = null;
-    	Class<?> type = innerType.type();
-    	for (Method m: type.getMethods()) {
-    		if (m.getAnnotation(ResourceKey.class) != null) {
-    			logger.debug("Using " + type.getName() + "." + m.getName() + " as resourceProvider for " + type + ".");
-    			resourceProvider = m;
-    		} else {
-    			logger.debug(m.getName() + " - " + m.getAnnotation(ResourceKey.class) + " - " + m.isAccessible());
-    		}
-    	}
-    	if (resourceProvider == null) {
-    		try {
-				resourceProvider = type.getMethod("toString", (Class<?>[])null);
-			} catch (SecurityException e) {
-				logger.error("toString() method of " + type + " can't be accessed.", e);
-			} catch (NoSuchMethodException e) {
-				logger.error("toString() method of " + type + " can't be found.", e);
-			}
-    	}
     	ILabelModelProvider<T> labelModelProvider;
-   		labelModelProvider = new StringResourceModelProvider<T>(resourceProvider, resourcePrefix);
+   		labelModelProvider = new StringResourceModelProvider<T>(resourcePrefix, innerType);
 
 		return labelModelProvider;
     }
